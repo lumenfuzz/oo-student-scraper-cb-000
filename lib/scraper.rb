0b0a89f63dd89b37f59cad8de8b5c1e6a534061e
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-16 10:57:58 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,28 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    student_info = doc.css(".card-text-container").text.split("\n").drop(1)
+    student_urls = doc.css(".student-card a")
+    student_hash_array = []
+    i = 0
+    student_urls.each do |student_url|
+      student_hash_array << {
+        :name => student_info[i*3].strip,
+        :location => student_info[i*3+1].strip,
+        :profile_url => student_url["href"]
+      }
+      i += 1
+    end
+    return student_hash_array
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
